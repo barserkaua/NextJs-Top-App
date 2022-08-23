d2fdef83c357a7fa7b0703c2a84078bd7b6f4589
Vasyl <barserkaua@gmail.com>
2022-08-23 17:28:32 +0300

    feat: :sparkles: installed eslint/import package & cleaned code

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from "../node_modules/next/dist/shared/lib/router/router";
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Htag } from "../components";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      Home
+      <Htag tag="h1">Some text</Htag>
+    </div>
+  );
+}
